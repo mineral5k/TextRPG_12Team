d0052f3885b41f5fa881037157ecf892a87cda3b
이지윤A <wldbs272@naver.com>
2024-09-26 18:03:52 +0900

    Revert "Merge branch 'dev' into LeeJiYun"
    This reverts commit f84d22758899acfa25743e63e1392ccf3abe10bb, reversing
    changes made to 70b964d7faa4144c8f8dc0fd026f1abca4e0e334.

--- TextRPG_Team12/Program.cs
+++ TextRPG_Team12/Program.cs
@@ -4,20 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("사용하실 닉네임을 입력하세요.");
-            string name = Console.ReadLine();
-
-            Console.WriteLine("\n원하시는 직업을 선택하세요.");
-            Console.WriteLine("1. 전사 2. 궁수 3. 도적 4. 마법사");
-
-       
-            // int jobChoice = GetInput();
-
-            // 번호 받아서 직업으로 배정  
-            string job = Console.ReadLine();
-
-            Console.WriteLine();
-            StartScene.startScene();
+            Console.WriteLine("Hello, World!");
         }
     }
 }
