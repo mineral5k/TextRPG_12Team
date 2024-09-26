e11bf7b1c31447874b2d8244a52bd89f0d948267
mineral5k <rkskek169@gmail.com>
2024-09-26 17:01:15 +0900

    Create SaveLoad.cs

---
+++ TextRPG_Team12/SaveLoad.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace TextRPG_Team12
+{
+    internal class SaveLoad
+    {
+        public static string path = AppDomain.CurrentDomain.BaseDirectory;
+
+        public void SaveData(Player player)
+        {
+            string playerData = JsonConvert.SerializeObject(player);
+            File.WriteAllText(path + "\\PlayerData.json", playerData);
+        }
+
+        public void LoadData(Player player)
+        {
+            if (!File.Exists(path + "\\PlayerData.json"))
+            {
+                return;
+            }
+
+            else
+            {
+                string playerLoadData = File.ReadAllText(path + "\\PlayerData.json");
+                player = JsonConvert.DeserializeObject<Player>(playerLoadData);
+            }
+        }
+
+
+
+
+    }
+}
