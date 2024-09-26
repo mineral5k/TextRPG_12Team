b82c9b794f821ba5a01cfbbac7ab7b39a899b6c1
udiladido <89720389+udiladido@users.noreply.github.com>
2024-09-26 16:55:13 +0900

    feat : "캐릭터, 적 구현"
    캐릭터, 적 클래스 구현

---
+++ TextRPG_Team12/ICharacter.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPG_Team12
+{
+    public interface ICharacter
+    {
+
+        
+        
+        string Name { get; }
+
+        int level { get; }
+        int Health { get; set; }
+        int AttackPower { get; set; }
+
+
+        public void TakeDamage() {
+
+            int a;
+        
+        }
+
+    }
+
+
+    public class Player : ICharacter
+    {
+
+        public string Name { get; }
+        public int level { get; }
+        public int Health { get; set; }
+        public int AttackPower { get; set; }
+
+        public void TakeDamage()
+        { 
+        
+        
+        
+        }
+
+
+    }
+
+
+    public class Monster : ICharacter
+    {
+     
+
+       
+        public string Name { get; }
+        public int level { get; }
+        public int Health { get; set; }
+        public int AttackPower { get; set; }
+
+  
+
+
+
+    }
+}
---
+++ TextRPG_Team12/Player.cs
@@ -0,0 +1,20 @@
+﻿namespace TextRPG_Team12
+{
+    public class Player : ICharacter
+    {
+
+        public string Name { get; }
+        public int level { get; }
+        public int Health { get; set; }
+        public int AttackPower { get; set; }
+
+        public void TakeDamage()
+        { 
+        
+        
+        
+        }
+
+
+    }
+}
