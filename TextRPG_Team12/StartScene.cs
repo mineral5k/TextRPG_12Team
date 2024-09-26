d291a83c9320d5a113b2f86c810dd4b96a746f2a
lsw7791 <lsw7791@naver.com>
2024-09-26 19:47:06 +0900

    Update StartScene.cs

--- TextRPG_Team12/StartScene.cs
+++ TextRPG_Team12/StartScene.cs
@@ -44,6 +44,37 @@ namespace TextRPG_Team12
         //        startScene(); // 유효하지 않은 입력일 경우 다시 시작
         //    }
         //}
+            //    if (int.TryParse(input, out int choice))
+            //    {
+            //        switch (choice)
+            //        {
+            //            case 1:
+            //                ShowStatus(); // 상태 보기 기능 호출
+            //                break;
+            //            case 2:
+            //                Inventory(); // 인벤토리 기능 호출
+            //                break;
+            //            case 3:
+            //                Shop(); // 상점 기능 호출
+            //                break;
+            //            case 4:
+            //                Dungeon dungeon = new Dungeon();
+            //                break;
+            //            case 0:
+            //                ExitGame(); // 게임 종료 기능 호출
+            //                break;
+            //            default:
+            //                Console.WriteLine("잘못된 입력입니다. 다시 선택해주세요.");
+            //                startScene(); // 잘못된 입력일 경우 다시 시작
+            //                break;
+            //        }
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("숫자를 입력해주세요.");
+            //        startScene(); // 유효하지 않은 입력일 경우 다시 시작
+            //    }
+            //}
 
         //// 상태보기
         //public static void ShowStatus()
@@ -53,6 +84,14 @@ namespace TextRPG_Team12
         //    // 이후 startScene()으로 돌아가기
         //    startScene();
         //}
+            //// 상태보기
+            //public static void ShowStatus()
+            //{
+            //    // 플레이어 상태를 출력하는 코드
+            //    Console.WriteLine("플레이어 상태를 출력합니다...");
+            //    // 이후 startScene()으로 돌아가기
+            //    startScene();
+            //}
 
         //// 인벤토리
         //public static void Inventory()
@@ -62,6 +101,14 @@ namespace TextRPG_Team12
         //    // 이후 startScene()으로 돌아가기
         //    startScene();
         //}
+            //// 인벤토리
+            //public static void Inventory()
+            //{
+            //    // 인벤토리 내용을 출력하고 관리하는 코드
+            //    Console.WriteLine("인벤토리를 출력합니다...");
+            //    // 이후 startScene()으로 돌아가기
+            //    startScene();
+            //}
 
         //// 상점
         //public static void Shop()
@@ -71,11 +118,28 @@ namespace TextRPG_Team12
         //    // 이후 startScene()으로 돌아가기
         //    startScene();
         //}
+            //// 상점
+            //public static void Shop()
+            //{
+            //    // 상점에서 아이템을 구매하는 코드
+            //    Console.WriteLine("상점을 출력합니다...");
+            //    // 이후 startScene()으로 돌아가기
+            //    startScene();
+            //}
 
         //// 게임 종료
         //public static void ExitGame()
         //{
         //    Console.WriteLine("게임을 종료합니다.");
     }
+            //// 게임 종료
+            //public static void ExitGame()
+            //{
+            //    Console.WriteLine("게임을 종료합니다.");
+
+            }
+        }
 
 
+
+    }
