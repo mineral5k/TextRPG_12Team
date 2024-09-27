a482f9f2480a72c06cca60f51088e0bd530931a9
mineral5k <rkskek169@gmail.com>
2024-09-27 12:12:49 +0900

    Revert "Add Num.Sel()"
    This reverts commit 3e584fa3b187191d67baa6a859e03f2de22e115a.

--- TextRPG_Team12/Program.cs
+++ TextRPG_Team12/Program.cs
@@ -21,44 +21,6 @@
         }
     }
 
-    public static class Num
-    {
-        public static int NumSel(int x)                    // 0~x 까지의 선택지를 선택하는 메서드
-        {
-            int a = 0;
-
-            Console.Write("원하시는 행동을 입력해 주세요 : ");
-
-            while (true)
-            {
-                try                                 //숫자 이외의 입력을 받았을 시의 예외처리
-                {
-                    a = int.Parse(Console.ReadLine());
-                }
-
-                catch (System.FormatException)                 // 숫자이외의 입력을 받았을 시 오류 메세지를 전송하고 다시 입력을 받음
-                {
-                    Console.WriteLine("잘못된 입력입니다.");
-                    continue;
-                }
-
-                if ((0 <= a) && (a <= x))              //입력받은 숫자가 선택지에 올바른지 검사
-                {
-                    break;                        //올바를 시 종료
-                }
-
-                else
-                {
-                    Console.WriteLine("잘못된 입력입니다.");        // 선택지 범위에 맞지 않은 숫자 입력을 받았을 시 오류 메세지 전송하고 다시 입력 받음
-                }
-
-            }
-
-            return a;                                //입력받은 값 반환
-
-        }
-    }
-
 
 
 }
