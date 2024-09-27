91e0f65ce7568ea5555f4caf206436bb6244eac9
mineral5k <rkskek169@gmail.com>
2024-09-27 16:53:27 +0900

    Update Program.cs

--- TextRPG_Team12/Program.cs
+++ TextRPG_Team12/Program.cs
@@ -6,19 +6,43 @@
 
         static void Main(string[] args)
         {
+            Player player;
             Console.WriteLine("사용하실 닉네임을 입력하세요.");
             string name = Console.ReadLine();
 
             Console.WriteLine("\n원하시는 직업을 선택하세요.");
             Console.WriteLine("1. 전사 2. 궁수 3. 도적 4. 마법사");
 
-       
-            // int jobChoice = GetInput();
 
+            bool repeat = false;
             // 번호 받아서 직업으로 배정  
-            string job = Console.ReadLine();
+            do
+            {
+                repeat = false;
 
-            Console.WriteLine();
+                int choice = Num.Sel(4);
+                switch (choice)
+                {
+
+                    case 0:
+                        Console.WriteLine("잘못된 입력입니다");
+                        repeat = true;
+                        break;
+                    case 1:
+                        player = new Player(name, new Worrior());
+                        break;
+                    case 2:
+                        player = new Player(name, new Archer());
+                        break;
+                    case 3:
+                        player = new Player(name, new Thief());
+                        break;
+                    case 4:
+                        player = new Player(name, new Mage());
+                        break;
+                }
+            }
+            while (repeat);
             StartScene.startScene();
         }
 
