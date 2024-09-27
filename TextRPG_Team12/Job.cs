c8a41ee989a0e36eae9ad3b4e079693a42f892cd
mineral5k <rkskek169@gmail.com>
2024-09-27 12:14:08 +0900

    Revert "Merge branch 'dev' into LeeJaeMin"
    This reverts commit 3399c4b4c504b9637edee703aca18903e5db4588, reversing
    changes made to 3e584fa3b187191d67baa6a859e03f2de22e115a.

--- TextRPG_Team12/Job.cs
+++ TextRPG_Team12/Job.cs
@@ -6,16 +6,21 @@ using System.Threading.Tasks;
 
 namespace TextRPG_Team12
 {
-    public class Job
+     public class Job
     {
+        public string JobName { get; set; }
         public int JobHealth { get; set; }
         public int JobMana { get; set; }
-        public int JobAttack { get; set; }
-        public int JobDefense { get; set; }
-        //치명타 스크립트에서 받아오기
-        public int JobCritical { get; set; }
-        //회피 스크립트에서 받아오기
-        public int JobEvasion { get; set; }
+        public int JobAttackPower { get; set; }
+        public int JobAmorDeffense { get; set; }
+        public string JobSkillName1 { get; set; }
+        public string JobSkillDesc1 { get; set; }
+        public string JobSkillName2 { get; set; }
+        public string JobSkillDesc2 { get; set; }
+        public string JobSkillName3 { get; set; }
+        public string JobSkillDesc3 { get; set; }
+
+
 
         public virtual void JobSkill_1()
         {
@@ -34,181 +39,147 @@ namespace TextRPG_Team12
 
     }
 
-    public class Warrior : Job
+     public class Worrior : Job
     {
-        public string JobSkillName1 { get; set; }
-        public string JobSkillName2 { get; set; }
-        public string JobSkillName3 { get; set; }
+       
 
-        public Warrior()
+       public Worrior()
         {
+            JobName = "전사";
             JobHealth = 0;
             JobMana = 0;
-            JobAttack = 0;
-            JobDefense = 0;
-            JobCritical = 0;
-            JobEvasion = 0;
+            JobAttackPower = 0;
+            JobAmorDeffense = 0;
+            JobSkillName1 = "";
+            JobSkillDesc1 = "";
+            JobSkillName2 = "";
+            JobSkillDesc2 = "";
+            JobSkillName3 = "";
+            JobSkillDesc3 = "";
 
-            JobSkillName1 = "찌르기"; // 단일기 스킬 이름 수정바람
-            JobSkillName2 = "휘두르기"; // 다중기 스킬 이름 수정바람
-            JobSkillName3 = "웅크리기"; // 방어력을 올리는 버프 이름 수정바람 
         }
+
+
 
         public override void JobSkill_1()
         {
-
-            int damage = (int)(JobAttack * 1.2);
-            Console.WriteLine($"{JobSkillName1}: 적에게 {damage}의 피해를 입혔습니다.");
 
         }
 
         public override void JobSkill_2()
         {
 
-            int damage = (int)(JobAttack * 0.8);
-            Console.WriteLine($"{JobSkillName2}: 여러 적에게 각각 {damage}의 피해를 입혔습니다.");
         }
 
         public override void JobSkill_3()
         {
-            int buffAmount = 10;
-            JobDefense += buffAmount;
-            Console.WriteLine($"{JobSkillName3}: 방어력이 {buffAmount}만큼 상승하여 현재 방어력은 {JobDefense}입니다.");
+
         }
 
     }
 
     public class Archer : Job
     {
-        public string JobSkillName1 { get; set; }
-        public string JobSkillName2 { get; set; }
-        public string JobSkillName3 { get; set; }
+      
 
         public Archer()
         {
+            JobName = "궁수";
             JobHealth = 0;
             JobMana = 0;
-            JobAttack = 0;
-            JobDefense = 0;
-            JobCritical = 0;
-            JobEvasion = 0;
-
-            JobSkillName1 = "크리티컬 샷"; // 단일기 
-            JobSkillName2 = "레인 애로우"; // 다수기 
-            JobSkillName3 = "포커스";     // 치명타 향상 버프
+            JobAttackPower = 0;
+            JobAmorDeffense = 0;
+            JobSkillName1 = " ";
+            JobSkillName2 = " ";
+            JobSkillName3 = " ";
         }
+
+
 
         public override void JobSkill_1()
         {
 
-            int damage = (int)(JobAttack * 1.4);
-            Console.WriteLine($"{JobSkillName1}: 적에게 {damage}의 치명상을 입혔습니다.");
         }
 
         public override void JobSkill_2()
         {
-            int damage = (int)(JobAttack * 0.8);
-            Console.WriteLine($"{JobSkillName2}: 여러 적에게 각각 {damage}의 피해를 입혔습니다.");
+
         }
 
         public override void JobSkill_3()
         {
 
-            int buffAmount = 0;
-            JobCritical *= buffAmount;
-            Console.WriteLine($"{JobSkillName3}: 치명타율이 {buffAmount}배 만큼 상승하여 현재 치명타율은 {JobCritical}입니다.");
         }
 
     }
 
     public class Thief : Job
     {
-        public string JobSkillName1 { get; set; }
-        public string JobSkillName2 { get; set; }
-        public string JobSkillName3 { get; set; }
+     
 
         public Thief()
         {
+            JobName = "도적";
             JobHealth = 0;
             JobMana = 0;
-            JobAttack = 0;
-            JobDefense = 0;
-            JobCritical = 0;
-            JobEvasion = 0;
-
-            JobSkillName1 = "표창 던지기";  // 단일기 
-            JobSkillName2 = "표창 난사";   // 다수기 
-            JobSkillName3 = "축지법";     // 회피율 버프 
+            JobAttackPower = 0;
+            JobAmorDeffense = 0;
+            JobSkillName1 = "";
+            JobSkillName2 = "";
+            JobSkillName3 = "";
         }
+
+
 
         public override void JobSkill_1()
         {
-            int damage = (int)(JobAttack * 1.3);
-            Console.WriteLine($"{JobSkillName1}: 적에게 {damage}의 피해를 입혔습니다.");
+
         }
 
         public override void JobSkill_2()
         {
 
-            int damage = (int)(JobAttack * 0.9);
-            Console.WriteLine($"{JobSkillName2}: 여러 적에게 각각 {damage}의 피해를 입혔습니다.");
         }
 
         public override void JobSkill_3()
         {
-            int buffAmount = 0;
-            JobEvasion *= buffAmount;
-            Console.WriteLine($"{JobSkillName3}: 회피율이 {buffAmount}배 만큼 상승하여 현재 회피율은 {JobEvasion}입니다.");
+
         }
 
         public class Mage : Job
         {
-            public string JobSkillName1 { get; set; }
-            public string JobSkillName2 { get; set; }
-            public string JobSkillName3 { get; set; }
+      
 
             public Mage()
             {
+                JobName = "마법사";
                 JobHealth = 0;
                 JobMana = 0;
-                JobAttack = 0;
-                JobDefense = 0;
-                JobCritical = 0;
-                JobEvasion = 0;
-
-                JobSkillName1 = "에너지 볼트"; // 단일기 
-                JobSkillName2 = "썬더 볼트";  // 다수기 
-                JobSkillName3 = "현자타임";  // 공격력 상승 버프
+                JobAttackPower = 0;
+                JobAmorDeffense = 0;
+                JobSkillName1 = "";
+                JobSkillName2 = "";
+                JobSkillName3 = "";
             }
+
+
 
             public override void JobSkill_1()
             {
 
-                int damage = (int)(JobAttack * 1.3);
-                Console.WriteLine($"{JobSkillName1}: 적에게 {damage}의 피해를 입혔습니다.");
             }
 
             public override void JobSkill_2()
             {
-                int damage = (int)(JobAttack * 0.9);
-                Console.WriteLine($"{JobSkillName2}: 여러 적에게 각각 {damage}의 피해를 입혔습니다.");
+
             }
 
             public override void JobSkill_3()
             {
 
-                int buffAmount = 0;
-                JobAttack += buffAmount;
-                Console.WriteLine($"{JobSkillName3}: 공격력이 {buffAmount}만큼 상승하여 현재 공격력은 {JobAttack}입니다.");
             }
 
         }
+
     }
 }
-
-       
-
-
-
-
-    
--- TextRPG_Team12/Player.cs
+++ TextRPG_Team12/Player.cs
@@ -1,22 +1,27 @@
-﻿namespace TextRPG_Team12
+﻿using System.Reflection.Metadata;
+
+namespace TextRPG_Team12
 {
     public class Player : ICharacter
     {
 
-        public static readonly int[] LevelUpExp = {0, 10, 35, 65, 100};
-     
+        public static readonly int[] LevelUpExp = { 0, 10, 35, 65, 100 };
+
 
 
         public string Name { get; }
 
-        public string Job{ get; set;}
+        Job job = new Job();
 
         public int Level { get; set; }
         public int Health { get; set; }
-        
+
         public int Gold { get; set; }
-        
+
         public float AttackPower { get; set; }
+
+        public int WeaponStat { get; set; }
+        public int AmorStat {get; set; }
 
         public int AmorDefense { get; set; }
 
@@ -41,17 +46,16 @@
 
 
 
-        public Player(string name, string job, int JobHealth, int JobAmorDefense, int JobAttackPower, int JobMana) {
-
-            // 초기 기본 스텟 설정 
+        public Player(string name, Job inputJob)
+        {
             Name = name;
-            Job = job;  
-            Health = JobHealth;
+            job = inputJob;  
             Level = 1;
-            Mana = JobMana;
-            AmorDefense = JobAmorDefense;
-            AttackPower = JobAttackPower;
             Gold = 1500;
+            Health = 100 +job.JobHealth;
+            Mana = 50 + job.JobMana;
+            AttackPower = 10 +job.JobAttackPower;
+            AmorDefense = 5 + job.JobAmorDeffense;
 
         }
 
