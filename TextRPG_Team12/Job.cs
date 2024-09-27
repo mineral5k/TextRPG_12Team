dba3832b5ea11e2d9d8264d7ef3f97cf97088335
lsw7791 <lsw7791@naver.com>
2024-09-27 11:36:14 +0900

    Update Job.cs

--- TextRPG_Team12/Job.cs
+++ TextRPG_Team12/Job.cs
@@ -12,6 +12,11 @@ namespace TextRPG_Team12
         public int JobMana { get; set; }
         public int JobAttack { get; set; }
         public int JobDeffense { get; set; }
+        public int JobDefense { get; set; }
+        //치명타 스크립트에서 받아오기
+        public int JobCritical { get; set; }
+        //회피 스크립트에서 받아오기
+        public int JobEvasion { get; set; }
 
         public virtual void JobSkill_1()
         {
@@ -31,12 +36,14 @@ namespace TextRPG_Team12
     }
 
     public class Worrior : Job
+    public class Warrior : Job
     {
         public string JobSkillName1 { get; set; }
         public string JobSkillName2 { get; set; }
         public string JobSkillName3 { get; set; }
 
         Worrior()
+        public Warrior()
         {
             JobHealth = 0;
             JobMana = 0;
@@ -47,21 +54,38 @@ namespace TextRPG_Team12
             JobSkillName3 = "";
         }
 
+            JobDefense = 0;
+            JobCritical = 0;
+            JobEvasion = 0;
 
+            JobSkillName1 = "찌르기"; // 단일기 스킬 이름 수정바람
+            JobSkillName2 = "휘두르기"; // 다중기 스킬 이름 수정바람
+            JobSkillName3 = "웅크리기"; // 방어력을 올리는 버프 이름 수정바람 
+        }
 
         public void JobSkill_1()
+        public override void JobSkill_1()
         {
 
+            int damage = (int)(JobAttack * 1.2); 
+            Console.WriteLine($"{JobSkillName1}: 적에게 {damage}의 피해를 입혔습니다.");
         }
 
         public void JobSkill_2()
+      
+        public override void JobSkill_2()
         {
 
+            int damage = (int)(JobAttack * 0.8); 
+            Console.WriteLine($"{JobSkillName2}: 여러 적에게 각각 {damage}의 피해를 입혔습니다.");
         }
 
         public void JobSkill_3()
+        public override void JobSkill_3()
         {
-
+            int buffAmount = 10; 
+            JobDefense += buffAmount;
+            Console.WriteLine($"{JobSkillName3}: 방어력이 {buffAmount}만큼 상승하여 현재 방어력은 {JobDefense}입니다.");
         }
 
     }
@@ -73,6 +97,7 @@ namespace TextRPG_Team12
         public string JobSkillName3 { get; set; }
 
         Archer()
+        public Archer()
         {
             JobHealth = 0;
             JobMana = 0;
@@ -83,21 +108,37 @@ namespace TextRPG_Team12
             JobSkillName3 = "";
         }
 
+            JobDefense = 0;
+            JobCritical = 0;
+            JobEvasion = 0;
 
+            JobSkillName1 = "크리티컬 샷"; // 단일기 
+            JobSkillName2 = "레인 애로우"; // 다수기 
+            JobSkillName3 = "포커스";     // 치명타 향상 버프
+        }
 
         public void JobSkill_1()
+        public override void JobSkill_1()
         {
 
+            int damage = (int)(JobAttack * 1.4);
+            Console.WriteLine($"{JobSkillName1}: 적에게 {damage}의 치명상을 입혔습니다.");
         }
 
         public void JobSkill_2()
+        public override void JobSkill_2()
         {
-
+            int damage = (int)(JobAttack * 0.8);
+            Console.WriteLine($"{JobSkillName2}: 여러 적에게 각각 {damage}의 피해를 입혔습니다.");
         }
 
         public void JobSkill_3()
+        public override void JobSkill_3()
         {
 
+            int buffAmount = 0;
+            JobCritical *= buffAmount;
+            Console.WriteLine($"{JobSkillName3}: 치명타율이 {buffAmount}배 만큼 상승하여 현재 치명타율은 {JobCritical}입니다.");
         }
 
     }
@@ -119,21 +160,36 @@ namespace TextRPG_Team12
             JobSkillName3 = "";
         }
 
+            JobDefense = 0;
+            JobCritical = 0;
+            JobEvasion = 0;
 
+            JobSkillName1 = "표창 던지기";  // 단일기 
+            JobSkillName2 = "표창 난사";   // 다수기 
+            JobSkillName3 = "축지법";     // 회피율 버프 
+        }
 
         public void JobSkill_1()
+        public override void JobSkill_1()
         {
-
+            int damage = (int)(JobAttack * 1.3);
+            Console.WriteLine($"{JobSkillName1}: 적에게 {damage}의 피해를 입혔습니다.");
         }
 
         public void JobSkill_2()
+        public override void JobSkill_2()
         {
 
+            int damage = (int)(JobAttack * 0.9);
+            Console.WriteLine($"{JobSkillName2}: 여러 적에게 각각 {damage}의 피해를 입혔습니다.");
         }
 
         public void JobSkill_3()
+        public override void JobSkill_3()
         {
-
+            int buffAmount = 0;
+            JobEvasion *= buffAmount;
+            Console.WriteLine($"{JobSkillName3}: 회피율이 {buffAmount}배 만큼 상승하여 현재 회피율은 {JobEvasion}입니다.");
         }
 
         public class Mage : Job
@@ -151,28 +207,45 @@ namespace TextRPG_Team12
                 JobSkillName1 = "";
                 JobSkillName2 = "";
                 JobSkillName3 = "";
+                JobDefense = 0;
+                JobCritical = 0;
+                JobEvasion = 0;
+
+                JobSkillName1 = "에너지 볼트"; // 단일기 
+                JobSkillName2 = "썬더 볼트";  // 다수기 
+                JobSkillName3 = "현자타임";  // 공격력 상승 버프
             }
 
 
 
             public void JobSkill_1()
+            public override void JobSkill_1()
             {
 
+                int damage = (int)(JobAttack * 1.3);
+                Console.WriteLine($"{JobSkillName1}: 적에게 {damage}의 피해를 입혔습니다.");
             }
 
             public void JobSkill_2()
+            public override void JobSkill_2()
             {
-
+                int damage = (int)(JobAttack * 0.9);
+                Console.WriteLine($"{JobSkillName2}: 여러 적에게 각각 {damage}의 피해를 입혔습니다.");
             }
 
             public void JobSkill_3()
+            public override void JobSkill_3()
             {
 
+                int buffAmount = 0;
+                JobAttack += buffAmount;
+                Console.WriteLine($"{JobSkillName3}: 공격력이 {buffAmount}만큼 상승하여 현재 공격력은 {JobAttack}입니다.");
             }
 
         }
 
 
+       
 
 
 
